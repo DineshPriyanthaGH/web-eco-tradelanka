2933d2b78e3d0b2d9bbc1e73d305840e41938b7e
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-07-10 09:44:58 +0530

    update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";    
+import { useState } from "react";    
 import { Menu, X, Leaf } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { QuoteForm } from "./QuoteForm";
